79f139a3d345385670508238a79d1a89efbd6dde
colecee <88739613+colecee@users.noreply.github.com>
2021-09-25 21:48:37 -0500

    Add files via upload

---
+++ Quiz Challenge/script.js
@@ -0,0 +1,110 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit')
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+            );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+          );
+        }
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
+    {
+        var questions= [
+            "What is Java Script Used For?"
+            "What does Javascript Allow you to do?"
+        ];
+        var answers = [
+            "a"
+            "c"
+        ]
+        }
+      var questionNumber1: "What is Java Script Used for?",
+      answers: {
+        a: "Make the HTML Work",
+        b: "To store data",
+      },
+      correctAnswer: "a"
+    },
+    {
+      var questionNumber2: "What does Javascript allow you to do?",
+      answers: {
+        a: "Create variables",
+        b: "Create Functions",
+        c: "Both a and b"
+      },
+      correctAnswer: "c"
+    }
+];
+
+// Kick things off
+buildQuiz();
+
+// Event listeners
+submitButton.addEventListener('click', showResults);
+})();
+ 
